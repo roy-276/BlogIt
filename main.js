8736414ee1f7c66dc9bbbe55d7c8cdbe62217c80
roy-276 <maingiroy95@gmail.com>
2023-03-03 17:04:18 +0300

    Dashboard Media Queries (Responsive Design)

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ const closeNav = () => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show_sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide_sidebar-btn');
+
+//show sidebar
+const showSidebar = () => {
+  sidebar.style.left = '0';
+  showSidebarBtn.style.display = 'none';
+  hideSidebarBtn.style.display = 'inline-block';
+};
+
+//hide sidebar
+const hideSidebar = () => {
+  sidebar.style.left = '-100%';
+  showSidebarBtn.style.display = 'inline-block';
+  hideSidebarBtn.style.display = 'none';
+};
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
